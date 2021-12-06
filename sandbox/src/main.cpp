ba149b77b1be18343e3ee9436d4a1bac41942f8b
Maksym Pasichnyk <maximmkaaa@gmail.com>
2021-12-07 00:28:06 +0200

    raymarching example

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -11,50 +11,107 @@
 #include <glm/glm.hpp>
 #include <spdlog/spdlog.h>
 
-static auto sphere(const glm::vec3& ro, const glm::vec3& rd, const glm::vec3& c, float r) -> float {
-    const auto L = c - ro;
-    const auto tca = glm::dot(L, rd);
-    const auto d2 = glm::dot(L, L) - tca * tca;
-    if (d2 > r * r) {
-        return -1.0f;
-    }
-    const auto thc = glm::sqrt(r * r - d2);
-    const auto t0 = tca - thc;
-    const auto t1 = tca + thc;
-    return t0 >= 0 ? t0 : t1 >= 0 ? t1 : -1.0f;
-}
-
 using Clock = std::chrono::high_resolution_clock;
 
-struct Game : Blaze::App {
-    Material _material;
+float Time;
+glm::vec2 Resolution;
+glm::vec3 LightPosition;
+glm::vec3 CameraPosition;
+glm::mat3 CameraRotation;
 
+static auto sdSmoothUnion(glm::f32 d1, glm::f32 d2, glm::f32 k) -> glm::f32 {
+    const auto h = glm::clamp(0.5f + 0.5f * (d2 - d1) / k, 0.0f, 1.0f);
+    return glm::mix(d2, d1, h) - k * h * (1.0f - h);
+}
+
+static auto sdSphere(const glm::vec3& c, const glm::vec3& p, glm::f32 r) -> glm::f32 {
+    return glm::length(c - p) - r;
+}
+
+static auto sdTorus(const glm::vec3& p, const glm::vec2& t) -> glm::f32 {
+    const auto q = glm::vec2(glm::length(p.xz()) - t.x, p.y);
+    return glm::length(q) - t.y;
+}
+
+static auto scene(const glm::vec3& p) -> glm::f32 {
+    auto d = sdTorus(p, glm::vec2(1.0f, 0.2f));
+    d = sdSmoothUnion(d, sdSphere(p, glm::vec3(0.0f, glm::sin(Time), 0.0f), 0.3f), 1.0f);
+    return d;
+}
+
+static auto camera(const glm::vec3& cameraPos, const glm::vec3& lookAtPoint) -> glm::mat3 {
+    const auto cd = glm::normalize(lookAtPoint - cameraPos);
+    const auto cr = glm::normalize(glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), cd));
+    const auto cu = glm::normalize(glm::cross(cd, cr));
+    return glm::mat3(-cr, cu, -cd);
+}
+
+static auto calcNormal(const glm::vec3& p) -> glm::vec3 {
+    const auto e = glm::vec2(1.0f, -1.0f) * 0.0005f;
+    return glm::normalize(
+        e.xyy() * scene(p + e.xyy()) +
+        e.yyx() * scene(p + e.yyx()) +
+        e.yxy() * scene(p + e.yxy()) +
+        e.xxx() * scene(p + e.xxx())
+    );
+}
+
+static auto raymarch(const glm::vec3& ro, const glm::vec3& rd) -> glm::f32 {
+    glm::f32 t = 0;
+    for (int i = 0; i < 64; ++i) {
+        const auto p = ro + rd * t;
+        const auto d = scene(p);
+        t += d;
+        if (d < 0.001 || t > 10000.0f) {
+            break;
+        }
+    }
+    return t;
+}
+
+static auto mainImage(const glm::vec2& fragCoord) -> glm::vec4 {
+    const auto uv = (fragCoord - 0.5f * Resolution) / Resolution.y;
+
+    const auto ro = CameraPosition;
+    const auto rd = CameraRotation * glm::normalize(glm::vec3(uv, -1.0f));
+
+    const auto sd = raymarch(ro, rd);
+    if (sd > 1000.0f) {
+        return glm::vec4(0);
+    }
+    const auto p = ro + rd * sd;
+    const auto n = calcNormal(p);
+    const auto l = glm::normalize(LightPosition - p);
+    const auto i = glm::clamp(glm::dot(n, l), 0.3f, 1.0f);
+    const auto c = glm::vec3(1.0f, 0.0f, 0.0f) * i;
+    return glm::vec4(c, 1.0f);
+}
+
+struct Game : Blaze::App {
+    Mesh _mesh;
+    Material _material;
     Texture2d _texture;
     vk::Sampler _sampler;
-    TextureData _textureData{800 * 4, 600 * 4};
+    TextureData _textureData{800, 600};
 
-    Mesh _mesh;
-    float _time = 0;
-    float _angle = 0;
+    float _updateTime = 0;
     bool _multithreading = true;
 
     void Init() override {
-        _material = Material::LoadFromResources("sandbox:materials/texture.material");
         _texture = Texture2d(_textureData.getExtent());
-
-        const auto samplerCreateInfo = vk::SamplerCreateInfo{
-//            .magFilter = vk::Filter::eLinear,
-//            .minFilter = vk::Filter::eLinear,
-//            .mipmapMode = vk::SamplerMipmapMode::eLinear,
+        _sampler = Blaze::GetLogicalDevice().createSampler(vk::SamplerCreateInfo{
+            .magFilter = vk::Filter::eLinear,
+            .minFilter = vk::Filter::eLinear,
+            .mipmapMode = vk::SamplerMipmapMode::eLinear,
             .addressModeU = vk::SamplerAddressMode::eRepeat,
             .addressModeV = vk::SamplerAddressMode::eRepeat,
             .addressModeW = vk::SamplerAddressMode::eRepeat,
             .maxAnisotropy = 1.0f,
             .minLod = -1000,
             .maxLod = 1000
-        };
+        }, nullptr);
 
-        _sampler = Blaze::GetLogicalDevice().createSampler(samplerCreateInfo, nullptr);
+        _material = Material::LoadFromResources("sandbox:materials/texture.material");
         _material.setTexture(0, _texture, _sampler);
 
         const auto vertices = std::array {
@@ -84,41 +141,30 @@ struct Game : Blaze::App {
     }
 
     void Update() override {
-        const auto extent = _textureData.getExtent();
-        const auto f_extent = glm::vec2(glm::uvec2(extent.width, extent.height));
-        const auto aspect = glm::vec2(f_extent.x, f_extent.y) / glm::vec2(f_extent.y, f_extent.y);
-
-
-        const auto ro = glm::vec3(0, 0, -3);
-        const auto light = glm::normalize(glm::vec3(glm::sin(_angle), 0.5f, glm::cos(_angle)));
-
         const auto time1 = Clock::now();
+        const auto extent = _textureData.getExtent();
+
+        Time += Blaze::GetDeltaTime();
+        Resolution = glm::vec2(glm::uvec2(extent.width, extent.height));
+        LightPosition = glm::vec3(
+            glm::sin(glm::radians(180.0f) * Time) * 2.0f,
+            0.5f,
+            glm::cos(glm::radians(180.0f) * Time) * 2.0f
+        );
+
+        CameraPosition = glm::vec3(5, 5, 5);
+        CameraRotation = camera(CameraPosition, glm::vec3(0));
 
         if (_multithreading) {
-            ThreadPool pool{};
+            auto pool = ThreadPool{};
 
             const auto xstep = extent.width;
             const auto ystep = 1;
 
             for (glm::u32 y = 0; y < extent.height; y += ystep) {
                 for (glm::u32 x = 0; x < extent.width; x += xstep) {
-                    pool.jobs.emplace([&, x0 = x, x1 = x + xstep, y0 = y, y1 = y + ystep] {
-                        for (glm::u32 y = y0; y < y1; ++y) {
-                            for (glm::u32 x = x0; x < x1; ++x) {
-                                _textureData.setPixel(x, y, glm::u8vec4(0));
-
-                                const auto uv = (glm::vec2(glm::uvec2(x, y)) / f_extent * 2.0f - 1.0f) * aspect;
-                                const auto rd = glm::normalize(glm::vec3(uv, 1));
-                                const auto s = sphere(ro, rd, glm::vec3(0), 1.0f);
-
-                                if (s >= 0.0f) {
-                                    const auto p = glm::normalize(ro + rd * s);
-                                    const auto i = glm::clamp(glm::dot(p, light), 0.0f, 1.0f) + 0.2F;
-                                    const auto c = glm::vec3(1.0f, 0.0f, 0.0f) * i;
-                                    _textureData.setPixel(x, y, glm::vec4(c, 1.0f));
-                                }
-                            }
-                        }
+                    pool.jobs.emplace([this, x0 = x, x1 = x + xstep, y0 = y, y1 = y + ystep] {
+                        FillTexture(x0, x1, y0, y1);
                     });
                 }
             }
@@ -126,29 +172,19 @@ struct Game : Blaze::App {
             pool.start();
             pool.wait();
         } else {
-            for (glm::u32 y = 0; y < extent.height; ++y) {
-                for (glm::u32 x = 0; x < extent.width; ++x) {
-                    _textureData.setPixel(x, y, glm::u8vec4(0));
-
-                    const auto uv = (glm::vec2(glm::uvec2(x, y)) / f_extent * 2.0f - 1.0f) * aspect;
-                    const auto rd = glm::normalize(glm::vec3(uv, 1));
-
-                    const auto s = sphere(ro, rd, glm::vec3(0), 1.0f);
-
-                    if (s >= 0.0f) {
-                        const auto p = glm::normalize(ro + rd * s);
-                        const auto i = glm::clamp(glm::dot(p, light), 0.0f, 1.0f) + 0.2F;
-                        const auto c = glm::vec3(1.0f, 0.0f, 0.0f) * i;
-                        _textureData.setPixel(x, y, glm::vec4(c,  1.0f));
-                    }
-                }
-            }
+            FillTexture(0, extent.width, 0, extent.height);
         }
         _texture.setPixels(_textureData.getPixels());
         const auto time2 = Clock::now();
+        _updateTime = static_cast<float>(Timestamp{time2 - time1}.seconds());
+    }
 
-        _time = static_cast<float>(Timestamp{time2 - time1}.seconds());
-        _angle += glm::radians(1.0f) * Blaze::GetDeltaTime() * 180.0f;
+    void FillTexture(glm::u32 x0, glm::u32 x1, glm::u32 y0, glm::u32 y1) {
+        for (glm::u32 y = y0; y < y1; ++y) {
+            for (glm::u32 x = x0; x < x1; ++x) {
+                _textureData.setPixel(x, y, mainImage(glm::vec2(x, y)));
+            }
+        }
     }
 
     void Render(CommandBuffer cmd) override {
@@ -160,7 +196,7 @@ struct Game : Blaze::App {
         ImGui::SetNextWindowSize(ImVec2(200, 100), ImGuiCond_Always);
         ImGui::Begin("Info");
         ImGui::TextUnformatted(fmt::format("DeltaTime: {:.3}s", Blaze::GetDeltaTime()).c_str());
-        ImGui::TextUnformatted(fmt::format("UpdateTexture: {:.3}s", _time).c_str());
+        ImGui::TextUnformatted(fmt::format("UpdateTime: {:.3}s", _updateTime).c_str());
         ImGui::Checkbox("Multithreading", &_multithreading);
         ImGui::End();
     }
